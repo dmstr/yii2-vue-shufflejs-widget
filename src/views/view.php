857492b2046969f0b0b9f7fe5c0590c322f3a685
Marc Mautz <m.mautz@herzogkommunikation.de>
2018-02-22 11:54:02 +0100

    removed class

--- src/views/view.php
+++ src/views/view.php
@@ -7,7 +7,7 @@
  * file that was distributed with this source code.
  */
 ?>
-<div class="container">
+<div>
     <div id="app">
         <vue-shuffle :settings="settings" :products="products"></vue-shuffle>
     </div>
